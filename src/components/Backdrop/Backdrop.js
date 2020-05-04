9aec99924429045c0bb98a6e19fe16c6f51909b6
Jamie95187 <jamiewong63@googlemail.com>
2020-05-04 09:34:40 +0100

    preparation

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    // console.log("[BackDrop] show status = " + props.show);
+
+    const cssClasses = [
+      'Backdrop',
+      props.show ? 'BackdropOpen' : 'BackdropClosed'
+    ];
+    return <div className={cssClasses.join(' ')}></div>
+
+};
 
 export default backdrop;
